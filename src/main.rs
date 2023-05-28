e879d8df2055b1e5273e1482c8dc6f860d8b777b
kingbultsea <894275157@qq.com>
2023-05-29 05:03:29 +0800

    🐶

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let handle = tokio::spawn(async {
+       10086
+    });
+
+    let out = handle.await.unwrap();
+    println!("GOT {}", out);
 }
